bc81af71cd4b5bb6e040356af06dc3082c478f5a
Andrew Duong <duongsters@gmail.com>
2019-10-24 18:21:34 -0700

    created connection to mongo DB

--- server.js
+++ server.js
@@ -6,20 +6,39 @@ var exphbs  = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+//---Initialize Express----
 var app = express();
 
+//connected 'db' variable to 'models' folder
 var db = require("./models");
 
-//----------Local host PORT setting
+//----------Local host PORT setting----------------
 var PORT = 8080;
 
-//----------Registering Express Handlebars view engine 
+//----------Registering Express Handlebars view engine --------
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-//----------Middleware Configuration
+//----------Middleware Configuration-----------
+    // Use morgan logger for logging requests
 app.use(logger("dev"));
+
+    // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+    // targeting the 'views' folder to be static
 app.use(express.static("views"));
+
+
+
+//---------Connection to MongoDB---------------
+mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
+
+
+
+
+
+//--------------Routes------------------------
+
 
