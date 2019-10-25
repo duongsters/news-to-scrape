f5ef4e65b510d63b0e836d1bac32e68d579fd0d3
Andrew Duong <duongsters@gmail.com>
2019-10-24 20:56:19 -0700

    posted all my GET route function calls to Hacker News

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var exphbs  = require("express-handlebars");
+var exphbs = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -20,14 +20,14 @@ app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 //----------Middleware Configuration-----------
-    // Use morgan logger for logging requests
+// Use morgan logger for logging requests
 app.use(logger("dev"));
 
-    // Parse request body as JSON
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-    // targeting the 'views' folder to be static
+// targeting the 'views' folder to be static
 app.use(express.static("views"));
 
 
@@ -37,8 +37,80 @@ mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
 
 
 
-
-
 //--------------Routes------------------------
+// When at the root route, this function will run 
+app.get("/", function (req, res) {
+    //to render the 'index.handlebars' template on to the homepage
+    res.render("index");
+});
+// Route for scraping all New Articles into application
+app.get("/scrape", function (req, res) {
+    //using axios, we grab the the body of hacker news' body of articles
+    axios.get("https://news.ycombinator.com/").then(function (response) {
+        // Loads that Hacker News' articles & links through cheerio then saves it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+
+        // Searches and grabs every class named 'title' within an table data tag then...
+        $("td.title").each(function (i, element) {
+            // console.log('\nnewElement');
+            // console.log(element);
+
+            //places empty result object into 'result' variable 
+            var result = {};
+
+            // Add the News Hacker article title and save it's as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            //finds the article url link by search for each a tag with href attributes to be saved in the result object
+            result.link = $(this)
+                .children("a")
+                .attr("href");
 
 
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added 'result' in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        });
+        //Sends a message to the user when the function is successfully ran to scrape Hacker News' articles
+        res.send("Hacker News's articles Scrape Complete");
+    });
+});
+
+//Route in scraping all Hacker News articles from the mongo DB
+app.get("/articles", function (req, res) {
+    //Finds and grabs every values from the 'Article' table
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for grabbing a specific Hacker News article by id of the user
+app.get("/articles/:id", function (req, res) {
+    // Using the user's 'id' passed in the id parameter,
+    // prepare a query that finds the matching ids ones within the mongo db
+    db.Article.findOne({ _id: req.params.id })
+        //populate it with it's note
+        .populate("note")
+        .then(function (dbArticle) {
+            //sends back the user's article back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
