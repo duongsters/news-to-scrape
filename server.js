89c5ff378263d52c74eb87424367494d4527d1cf
Andrew Duong <duongsters@gmail.com>
2019-10-24 18:11:52 -0700

    added cheerio and axios npm...also created calls to express and connected server.js to require the models folder...set PORT for localhost...and created handlebars calls through express

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+// -------Dependencies---------
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs  = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var app = express();
+
+var db = require("./models");
+
+var PORT = 8080;
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
