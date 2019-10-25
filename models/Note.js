a1d8f760db679cd7f0870834a165297fa449b521
Andrew Duong <duongsters@gmail.com>
2019-10-24 21:28:01 -0700

    added commentary

--- models/Note.js
+++ models/Note.js
@@ -1,12 +1,20 @@
+//use of mongoose
 var mongoose = require("mongoose");
+
+// Setting Schema constructor to 'Schema'
 var Schema = mongoose.Schema;
 
+// Similar toa Sequelize model, create a new NoteSchema object then...
 var NoteSchema = new Schema({
+    //gives the new NoteSchema the string value types of the title
     title: String,
+    //gives the new NoteSchema the string value types of the body
     body: String
 });
 
+
+//creates our model from the above schema, using mongoose's model method
 var Note = mongoose.model("Note", NoteSchema);
 
-
+//exports 'Note.js' model file
 module.exports = Note;
